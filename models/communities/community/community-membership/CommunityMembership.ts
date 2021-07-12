449b84dc76eda37973de51b33862265f8d67957d
Ashley Guevara <ashleyngp@gmail.com>
2021-07-12 15:54:40 -0500

    Update community membership models

--- models/communities/community/community-membership/CommunityMembership.ts
+++ models/communities/community/community-membership/CommunityMembership.ts
@@ -1,5 +1,7 @@
+import Color from 'color';
 import { DataModel } from '~/models/abstract/DataModel';
 import { DataModelAttributeMap } from '~/models/abstract/IDataModel';
+import { colorDeserializer, colorSerializer } from '~/models/common/serializers';
 import { ModelData } from '~/types/models-data/ModelData';
 import { ICommunityMembership } from './ICommunityMembership';
 
@@ -10,6 +12,9 @@ export class CommunityMembership extends DataModel<CommunityMembership> implemen
     communityId: number;
     isAdministrator: boolean;
     isModerator: boolean;
+    color: Color;
+    title: string;
+    name: string;
 
     dataMaps: DataModelAttributeMap<ICommunityMembership>[] = [
         {
@@ -19,6 +24,18 @@ export class CommunityMembership extends DataModel<CommunityMembership> implemen
         {
             dataKey: 'community_id',
             attributeKey: 'communityId',
+        },
+        {
+            dataKey: 'community_color',
+            attributeKey: 'color',
+        },
+        {
+            dataKey: 'community_title',
+            attributeKey: 'title',
+        },
+        {
+            dataKey: 'community_name',
+            attributeKey: 'name',
         },
         {
             dataKey: 'is_administrator',
