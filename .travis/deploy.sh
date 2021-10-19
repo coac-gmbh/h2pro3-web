61cbf8d718259f78e2423ba4a5178ce01c0e9447
Edison Arango <edisonarab@gmail.com>
2021-10-19 10:35:15 -0500

    Travis/ssl verify false

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,6 +3,7 @@
 # start the ssh agent
 eval "$(ssh-agent -s)"
 
+git config --local http.sslVerify false
 git remote add deploy https://dev.coac.de/okuna-web.git
 
 # force push to dokku master from develop branch
