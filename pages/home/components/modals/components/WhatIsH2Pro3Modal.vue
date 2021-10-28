abaf851c4a12034e3907817dbbf0f95eb29966ef
Edison Arango <edisonarab@gmail.com>
2021-10-27 21:30:31 -0500

    add what is h2pro3 modal

---
+++ pages/home/components/modals/components/WhatIsH2Pro3Modal.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="is-flex justify-center align-items-center has-height-100-percent has-padding-20">
+        <div class="ok-has-background-primary is-semi-rounded is-relative has-height-100-percent has-padding-30">
+            <h1 class="is-size-3 has-text-rounded ok-has-text-primary-invert">
+                Was ist <span v-html="$t('name_html')"></span>
+            </h1>
+            <p>
+                Mit <span v-html="$t('name_html')"></span> sollen sich Unternehmen, Kommunen und Institutionen zum Thema Wasserstoff und Sektorenkopplung digital miteinander vernetzen. Dadurch wird der Zugang zu Kompetenzträgern und Partnern aus Wirtschaft und Wissenschaft erleichtert sowie die Realisierung innovativer Projekte in den Bereichen Wasserstofferzeugung, -verteilung und -nutzung und -verteilung begünstigt. Gleichzeitig werden auf der Plattform Wissen aus Forschung und Technik, Know-How aus Wirtschaft und Kommunen sowie Erfahrungen bereits umgesetzter Projekte gebündelt und mit anderen Nutzern geteilt. Darüber hinaus soll <span v-html="$t('name_html')"></span> der interessierten Öffentlichkeit als Informationsquelle dienen, um sich über die Grundlagen und Nutzungsmöglichkeiten der Wasserstofftechnologie sowie über Anwendererfahrungen zu informieren.
+            </p>
+            <p>
+                <span v-html="$t('name_html')"></span> richtet in der Startphase seinen besonderen Fokus auf das Rheinische Revier, um als „Enabler“ die Wasserstofftechnologie in dieser Region voranzutreiben und damit einen unverzichtbaren Energieträger für die Energiewende zu etablieren. Die Umsetzung innovativer Projekte mit Wasserstofftechnologie steigert die Chancen für neue Geschäftsmodelle oder Produkte. Die daraus generierte Wertschöpfung führt zu neuen Arbeitsplätzen und leistet einen wichtigen Beitrag zum Strukturwandel der durch den Braunkohlenbergbau geprägten Region. Durch die bereits bestehende Energie-Infrastruktur sowie die bereits angesiedelten Unternehmen und Forschungseinrichtungen bietet das Rheinische Revier beste Voraussetzungen, um Vorzeigeregion im Bereich Wasserstofftechnologie zu werden. Davon werden auch andere Regionen profitieren, die über <span v-html="$t('name_html')"></span> Know-How und Erfahrungswerte abgreifen können.
+            </p>
+        </div>
+    </div>
+</template>
