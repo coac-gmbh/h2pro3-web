b671481b9d94001eecba51b9e220efbfcdcf9d05
Edison Arango <edisonarab@gmail.com>
2021-10-27 21:30:07 -0500

    add about us modal

---
+++ pages/home/components/modals/components/AboutUsModal.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="is-flex justify-center align-items-center has-height-100-percent has-padding-20">
+        <div class="ok-has-background-primary is-semi-rounded is-relative has-height-100-percent has-padding-30">
+            <h1 class="is-size-3 has-text-rounded ok-has-text-primary-invert">
+                Über uns – Entwickler und Förderer von <span v-html="$t('name_html')"></span>
+            </h1>
+            <p>
+                <span v-html="$t('name_html')"></span> wird durch das Konsortium der Partner coac GmbH, HyCologne – Wasserstoff Rheinland e.V. und Technische Hochschule Köln entwickelt. Die Maßnahme wird durch das Bundesmodellvorhaben „Unternehmen Revier“ für Maßnahmen zur Strukturanpassung in Braunkohlebergbauregionen gefördert. Die Vorbereitungen wurden u. a. auch durch die Zukunftsagentur Rheinisches Revier, das Wuppertal Institut für Klima, Umwelt, Energie gGmbH und die Provadis Hochschule sowie durch ENGIE Lab Environment & Society und Climate KIC im Rahmen des Pathfinder-Projektes HyDistrict aus der Flagshipmaßnahme Re-Industrialise unterstützt. Der Think-Tank „akkuraum“ der coac GmbH diente als Ort für die methodische Entwicklung des Basiskonzepts.
+            </p>
+        </div>
+    </div>
+</template>
